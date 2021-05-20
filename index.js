04ee3e6a4b731473402a7aee3ee68e287deceb08
Syed Andleeb <>
2021-05-20 13:37:09 +0000

    update : configured server ,  listening at PORT 3000

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const {DbConnection} = require('./db/DbConnection')
-
+const app = express()
+const PORT = 3000
 // initialize database connection
 DbConnection()
 
+
+
+app.get('/' , (req , res) =>{
+  res.json({status : true , message : "welcome to homepage"})
+})
+
+
+app.listen(PORT , () => console.log("Server up and running at " , PORT))
