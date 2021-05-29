6bfd2f55736261a133ed6dc79cf7bad4518e601e
Syed Andleeb <>
2021-05-29 14:28:04 +0000

    update : added route to update user's email and username

--- routes/account-route.js
+++ routes/account-route.js
@@ -13,7 +13,37 @@ AccountRoute.route('/')
  catch(error){
    res.json({status : false ,  message : "couldn.t fetch the user" , errMessage : error.messsage})
  }
-} )
+})
+
+AccountRoute.route('/update')
+.post(async (req , res) =>{
+  try{
+    const {userId, newUsername  , newEmail} = req.body
+  
+    async function checkForExistingEmail(){
+    const ifEmailPresent = await Users.findOne({email : newEmail})
+    if(ifEmailPresent){
+      throw {message : "Couldn't Update, email already linked to a different account"}
+    }
+  }
+
+   async function checkForExistingUserName(){
+    const ifUsernamePresent = await Users.findOne({username : newUsername})
+    if(ifUsernamePresent){
+      throw {message : "Couldn't Update, username already exists"}
+    }
+  }
+    await checkForExistingEmail()
+    await checkForExistingUserName()
+    
+   const response = await Users.findOneAndUpdate({_id : userId} , {username : newUsername , email : newEmail} , {new : true})
+   res.json({status : true , message : "updated user successfully" , response})
+  }
+  catch(error){
+    res.status(500).json({status : false , message : "couldn't update user" , errMessage : error.message})
+  }
+  
+})
 
 
 module.exports = {AccountRoute}
